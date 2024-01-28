f01bcab2b727a63168103b695123402a1acdfb76
soongjamm <soongjamm.dev@gmail.com>
2024-01-29 01:25:56 +0900

    init dot files

---
+++ Brewfile
@@ -0,0 +1,8 @@
+tap 'homebrew/cask'
+
+brew 'git'
+brew 'docker-compose'
+brew 'alacritty'
+brew 'fish'
+brew 'tmux'
+brew 'jq'
---
+++ Fish/config.fish
@@ -0,0 +1,8 @@
+set -U fish_user_paths /opt/homebrew/bin/ $fish_user_paths
+
+abbr ga "git add ." 
+abbr gca "git commit -av"
+abbr gp "git push"
+abbr gl "git pull"
+abbr sf "source ~/.config/fish/config.fish"
+
